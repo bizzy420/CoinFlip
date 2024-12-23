0673fffb99867484cab0c29168e3d6def948945b
eisenmond <eisenmond@outlook.com>
2024-12-22 20:59:26 -0500

    yolo

---
+++ CoinFlip.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CoinFlipToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("CoinFlip", "FLIP") {
+        // Mint the initial supply to the deployer
+        _mint(msg.sender, initialSupply);
+    }
+}
+
