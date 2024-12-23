6a27707e17d0301ba8cc7ca8c6479c9a4e4ff6a1
bizzy420 <animalsdoingcutestuff@gmail.com>
2024-12-23 02:18:00 +0000

    yolo

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -5,8 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CoinFlipToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("CoinFlip", "FLIP") {
-            // Mint the initial supply to the deployer
+            // Mint the initial supply to the deployer yolo
                     _mint(msg.sender, initialSupply);
                         }
                         }
-                        
